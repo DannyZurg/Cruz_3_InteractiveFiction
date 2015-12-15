12364b59750acc3c67762bd6cd6357aa8f5baf31
DannyZurg <DannyZurg@ninja14.d22.lan>
2015-12-15 08:09:28 -0600

    This is my Interactive Game

---
+++ src/cruz_3_interactivefiction/Choose.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cruz_3_interactivefiction;
+
+/**
+ *
+ * @author DannyZurg
+ */
+class Choose {
+    
+}
